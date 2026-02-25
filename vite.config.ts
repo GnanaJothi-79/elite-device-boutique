71c6f4e56031b35f98f542f8ff49d724697d3f8e
Gnana Jothi.G <gnana07092004@gmail.com>
2026-02-25 20:49:33 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+    base: "/elite-device-boutique/", 
   server: {
     host: "::",
     port: 8080,
